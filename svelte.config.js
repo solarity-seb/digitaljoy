699e345a143f969462c8ef556c14be33eb547aef
Seb Jagoe <seb@Mac.lan>
2025-03-28 17:37:00 -0400

    Disable prerendering lol

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,8 @@
 import adapter from '@sveltejs/adapter-netlify';
 
-/** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter(),
-    prerender: {
-      crawl: true,
-      entries: ['*'],
-      onError: 'continue'
-    }
+    adapter: adapter()
   }
 };
 
